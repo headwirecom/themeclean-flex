668481daa0918c4b99e30a3c0c9ab593318adaf4
reusr1 <ruben.reusser@mirumagency.com>
2019-04-22 09:41:54 -0700

    updated files

---
+++ core/src/main/java/com/themecleanflex/models/ArticleheaderModel.java
@@ -0,0 +1,422 @@
+package com.themeclean.models;
+
+import com.peregrine.nodetypes.models.AbstractComponent;
+import com.peregrine.nodetypes.models.IComponent;
+import com.peregrine.nodetypes.models.Container;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
+import org.apache.sling.models.annotations.Model;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/*
+    //GEN[:DATA
+    {
+  "definitions": {
+    "Articleheader": {
+      "type": "object",
+      "x-type": "component",
+      "properties": {
+        "showtitle": {
+          "type": "string",
+          "x-source": "inject",
+          "x-form-label": "Show Title",
+          "x-form-type": "materialswitch"
+        },
+        "title": {
+          "type": "string",
+          "x-source": "inject",
+          "x-form-label": "Title",
+          "x-form-visible": "model.showtitle == 'true'",
+          "x-form-type": "text"
+        },
+        "showsubtitle": {
+          "type": "string",
+          "x-source": "inject",
+          "x-form-label": "Show Subtitle",
+          "x-form-type": "materialswitch"
+        },
+        "subtitle": {
+          "type": "string",
+          "x-source": "inject",
+          "x-form-label": "Subtitle",
+          "x-form-visible": "model.showsubtitle == 'true'",
+          "x-form-rows": 3,
+          "x-form-type": "textarea"
+        },
+        "textalign": {
+          "type": "string",
+          "x-source": "inject",
+          "x-form-label": "Text align",
+          "x-form-type": "materialradio",
+          "properties": {
+            "left": {
+              "x-form-name": "Left",
+              "x-form-value": "text-left"
+            },
+            "center": {
+              "x-form-name": "Center",
+              "x-form-value": "text-center"
+            },
+            "right": {
+              "x-form-name": "Right",
+              "x-form-value": "text-right"
+            }
+          }
+        },
+        "bgref": {
+          "x-form-type": "reference",
+          "type": "object",
+          "x-type": "component",
+          "properties": {
+            "anchorname": {
+              "type": "string",
+              "x-source": "inject",
+              "x-form-label": "Anchor Name",
+              "x-form-type": "text"
+            },
+            "colorscheme": {
+              "type": "string",
+              "x-source": "inject",
+              "x-form-label": "Block Color Scheme",
+              "x-form-type": "materialradio",
+              "x-default": "",
+              "properties": {
+                "none": {
+                  "x-form-name": "None",
+                  "x-form-value": ""
+                },
+                "light": {
+                  "x-form-name": "Light",
+                  "x-form-value": "light"
+                },
+                "dark": {
+                  "x-form-name": "Dark",
+                  "x-form-value": "dark"
+                }
+              }
+            },
+            "custombackground": {
+              "type": "string",
+              "x-source": "inject",
+              "x-form-label": "Custom Background",
+              "x-form-type": "materialswitch",
+              "x-default": "false"
+            },
+            "backgroundtype": {
+              "type": "string",
+              "x-source": "inject",
+              "x-form-label": "Background Type",
+              "x-form-type": "materialradio",
+              "x-form-visible": "model.custombackground == 'true'",
+              "properties": {
+                "color": {
+                  "x-form-name": "Color",
+                  "x-form-value": "color"
+                },
+                "gradient": {
+                  "x-form-name": "Gradient",
+                  "x-form-value": "gradient"
+                },
+                "image": {
+                  "x-form-name": "Image",
+                  "x-form-value": "image"
+                },
+                "video": {
+                  "x-form-name": "Video",
+                  "x-form-value": "video"
+                }
+              }
+            },
+            "bgvideo": {
+              "type": "string",
+              "x-source": "inject",
+              "x-form-label": "Background Video",
+              "x-form-type": "pathbrowser",
+              "x-form-visible": "model.backgroundtype == 'video' and model.custombackground == 'true'",
+              "x-default": "https://www.youtube.com/embed/Ju86mknumYM",
+              "x-form-browserRoot": "/content/assets"
+            },
+            "bgimage": {
+              "type": "string",
+              "x-source": "inject",
+              "x-form-label": "Background Image",
+              "x-form-type": "pathbrowser",
+              "x-form-visible": "model.backgroundtype == 'image' and model.custombackground == 'true'",
+              "x-form-browserRoot": "/content/assets"
+            },
+            "overlay": {
+              "type": "string",
+              "x-source": "inject",
+              "x-form-label": "Overlay",
+              "x-form-type": "materialswitch",
+              "x-form-visible": "model.backgroundtype == 'image' and model.custombackground == 'true'"
+            },
+            "overlaycolor": {
+              "type": "string",
+              "x-source": "inject",
+              "x-form-label": "Overlay Color",
+              "x-form-type": "color",
+              "x-form-visible": "model.overlay == 'true' and model.backgroundtype == 'image' and model.custombackground == 'true'",
+              "x-default": "#ffffff"
+            },
+            "overlayopacity": {
+              "type": "string",
+              "x-source": "inject",
+              "x-form-label": "Overlay opacity",
+              "x-form-type": "materialrange",
+              "x-form-min": 0,
+              "x-form-max": 100,
+              "x-form-visible": "model.overlay == 'true' and model.backgroundtype == 'image' and model.custombackground == 'true'",
+              "x-default": "50"
+            },
+            "bgcolor": {
+              "type": "string",
+              "x-source": "inject",
+              "x-form-label": "Background Color",
+              "x-form-type": "color",
+              "x-form-visible": "(model.backgroundtype == 'color' or model.backgroundtype == 'gradient') and model.custombackground == 'true'",
+              "x-default": "#ffffff"
+            },
+            "color2": {
+              "type": "string",
+              "x-source": "inject",
+              "x-form-label": "Color 2",
+              "x-form-type": "color",
+              "x-form-visible": "model.backgroundtype == 'gradient' and model.custombackground == 'true'",
+              "x-default": "#c0c0c0"
+            },
+            "toppadding": {
+              "type": "string",
+              "x-source": "inject",
+              "x-form-label": "Top Padding",
+              "x-form-type": "materialrange",
+              "x-form-min": 0,
+              "x-form-max": 120,
+              "x-form-visible": "model.fullheight != 'true'"
+            },
+            "bottompadding": {
+              "type": "string",
+              "x-source": "inject",
+              "x-form-label": "Bottom Padding",
+              "x-form-type": "materialrange",
+              "x-form-min": 0,
+              "x-form-max": 120,
+              "x-form-visible": "model.fullheight != 'true'"
+            }
+          }
+        }
+      }
+    }
+  },
+  "name": "Articleheader",
+  "componentPath": "themecleanflex/components/articleheader",
+  "package": "com.themecleanflex.models",
+  "modelName": "Articleheader",
+  "classNameParent": "AbstractComponent"
+}
+//GEN]
+*/
+
+//GEN[:DEF
+@Model(
+        adaptables = Resource.class,
+        resourceType = "themecleanflex/components/articleheader",
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL,
+        adapters = IComponent.class
+)
+@Exporter(
+        name = "jackson",
+        extensions = "json"
+)
+
+//GEN]
+public class ArticleheaderModel extends AbstractComponent {
+
+    public ArticleheaderModel(Resource r) { super(r); }
+
+    //GEN[:INJECT
+    	/* {"type":"string","x-source":"inject","x-form-label":"Show Title","x-form-type":"materialswitch"} */
+	@Inject
+	private String showtitle;
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Title","x-form-visible":"model.showtitle == 'true'","x-form-type":"text"} */
+	@Inject
+	private String title;
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Show Subtitle","x-form-type":"materialswitch"} */
+	@Inject
+	private String showsubtitle;
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Subtitle","x-form-visible":"model.showsubtitle == 'true'","x-form-rows":3,"x-form-type":"textarea"} */
+	@Inject
+	private String subtitle;
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Text align","x-form-type":"materialradio","properties":{"left":{"x-form-name":"Left","x-form-value":"text-left"},"center":{"x-form-name":"Center","x-form-value":"text-center"},"right":{"x-form-name":"Right","x-form-value":"text-right"}}} */
+	@Inject
+	private String textalign;
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Anchor Name","x-form-type":"text"} */
+	@Inject
+	private String anchorname;
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Block Color Scheme","x-form-type":"materialradio","x-default":"","properties":{"none":{"x-form-name":"None","x-form-value":""},"light":{"x-form-name":"Light","x-form-value":"light"},"dark":{"x-form-name":"Dark","x-form-value":"dark"}}} */
+	@Inject
+	@Default(values ="")
+	private String colorscheme;
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Custom Background","x-form-type":"materialswitch","x-default":"false"} */
+	@Inject
+	@Default(values ="false")
+	private String custombackground;
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Background Type","x-form-type":"materialradio","x-form-visible":"model.custombackground == 'true'","properties":{"color":{"x-form-name":"Color","x-form-value":"color"},"gradient":{"x-form-name":"Gradient","x-form-value":"gradient"},"image":{"x-form-name":"Image","x-form-value":"image"},"video":{"x-form-name":"Video","x-form-value":"video"}}} */
+	@Inject
+	private String backgroundtype;
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Background Video","x-form-type":"pathbrowser","x-form-visible":"model.backgroundtype == 'video' and model.custombackground == 'true'","x-default":"https://www.youtube.com/embed/Ju86mknumYM","x-form-browserRoot":"/content/assets"} */
+	@Inject
+	@Default(values ="https://www.youtube.com/embed/Ju86mknumYM")
+	private String bgvideo;
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Background Image","x-form-type":"pathbrowser","x-form-visible":"model.backgroundtype == 'image' and model.custombackground == 'true'","x-form-browserRoot":"/content/assets"} */
+	@Inject
+	private String bgimage;
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Overlay","x-form-type":"materialswitch","x-form-visible":"model.backgroundtype == 'image' and model.custombackground == 'true'"} */
+	@Inject
+	private String overlay;
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Overlay Color","x-form-type":"color","x-form-visible":"model.overlay == 'true' and model.backgroundtype == 'image' and model.custombackground == 'true'","x-default":"#ffffff"} */
+	@Inject
+	@Default(values ="#ffffff")
+	private String overlaycolor;
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Overlay opacity","x-form-type":"materialrange","x-form-min":0,"x-form-max":100,"x-form-visible":"model.overlay == 'true' and model.backgroundtype == 'image' and model.custombackground == 'true'","x-default":"50"} */
+	@Inject
+	@Default(values ="50")
+	private String overlayopacity;
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Background Color","x-form-type":"color","x-form-visible":"(model.backgroundtype == 'color' or model.backgroundtype == 'gradient') and model.custombackground == 'true'","x-default":"#ffffff"} */
+	@Inject
+	@Default(values ="#ffffff")
+	private String bgcolor;
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Color 2","x-form-type":"color","x-form-visible":"model.backgroundtype == 'gradient' and model.custombackground == 'true'","x-default":"#c0c0c0"} */
+	@Inject
+	@Default(values ="#c0c0c0")
+	private String color2;
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Top Padding","x-form-type":"materialrange","x-form-min":0,"x-form-max":120,"x-form-visible":"model.fullheight != 'true'"} */
+	@Inject
+	private String toppadding;
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Bottom Padding","x-form-type":"materialrange","x-form-min":0,"x-form-max":120,"x-form-visible":"model.fullheight != 'true'"} */
+	@Inject
+	private String bottompadding;
+
+
+//GEN]
+
+    //GEN[:GETTERS
+    	/* {"type":"string","x-source":"inject","x-form-label":"Show Title","x-form-type":"materialswitch"} */
+	public String getShowtitle() {
+		return showtitle;
+	}
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Title","x-form-visible":"model.showtitle == 'true'","x-form-type":"text"} */
+	public String getTitle() {
+		return title;
+	}
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Show Subtitle","x-form-type":"materialswitch"} */
+	public String getShowsubtitle() {
+		return showsubtitle;
+	}
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Subtitle","x-form-visible":"model.showsubtitle == 'true'","x-form-rows":3,"x-form-type":"textarea"} */
+	public String getSubtitle() {
+		return subtitle;
+	}
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Text align","x-form-type":"materialradio","properties":{"left":{"x-form-name":"Left","x-form-value":"text-left"},"center":{"x-form-name":"Center","x-form-value":"text-center"},"right":{"x-form-name":"Right","x-form-value":"text-right"}}} */
+	public String getTextalign() {
+		return textalign;
+	}
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Anchor Name","x-form-type":"text"} */
+	public String getAnchorname() {
+		return anchorname;
+	}
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Block Color Scheme","x-form-type":"materialradio","x-default":"","properties":{"none":{"x-form-name":"None","x-form-value":""},"light":{"x-form-name":"Light","x-form-value":"light"},"dark":{"x-form-name":"Dark","x-form-value":"dark"}}} */
+	public String getColorscheme() {
+		return colorscheme;
+	}
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Custom Background","x-form-type":"materialswitch","x-default":"false"} */
+	public String getCustombackground() {
+		return custombackground;
+	}
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Background Type","x-form-type":"materialradio","x-form-visible":"model.custombackground == 'true'","properties":{"color":{"x-form-name":"Color","x-form-value":"color"},"gradient":{"x-form-name":"Gradient","x-form-value":"gradient"},"image":{"x-form-name":"Image","x-form-value":"image"},"video":{"x-form-name":"Video","x-form-value":"video"}}} */
+	public String getBackgroundtype() {
+		return backgroundtype;
+	}
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Background Video","x-form-type":"pathbrowser","x-form-visible":"model.backgroundtype == 'video' and model.custombackground == 'true'","x-default":"https://www.youtube.com/embed/Ju86mknumYM","x-form-browserRoot":"/content/assets"} */
+	public String getBgvideo() {
+		return bgvideo;
+	}
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Background Image","x-form-type":"pathbrowser","x-form-visible":"model.backgroundtype == 'image' and model.custombackground == 'true'","x-form-browserRoot":"/content/assets"} */
+	public String getBgimage() {
+		return bgimage;
+	}
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Overlay","x-form-type":"materialswitch","x-form-visible":"model.backgroundtype == 'image' and model.custombackground == 'true'"} */
+	public String getOverlay() {
+		return overlay;
+	}
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Overlay Color","x-form-type":"color","x-form-visible":"model.overlay == 'true' and model.backgroundtype == 'image' and model.custombackground == 'true'","x-default":"#ffffff"} */
+	public String getOverlaycolor() {
+		return overlaycolor;
+	}
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Overlay opacity","x-form-type":"materialrange","x-form-min":0,"x-form-max":100,"x-form-visible":"model.overlay == 'true' and model.backgroundtype == 'image' and model.custombackground == 'true'","x-default":"50"} */
+	public String getOverlayopacity() {
+		return overlayopacity;
+	}
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Background Color","x-form-type":"color","x-form-visible":"(model.backgroundtype == 'color' or model.backgroundtype == 'gradient') and model.custombackground == 'true'","x-default":"#ffffff"} */
+	public String getBgcolor() {
+		return bgcolor;
+	}
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Color 2","x-form-type":"color","x-form-visible":"model.backgroundtype == 'gradient' and model.custombackground == 'true'","x-default":"#c0c0c0"} */
+	public String getColor2() {
+		return color2;
+	}
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Top Padding","x-form-type":"materialrange","x-form-min":0,"x-form-max":120,"x-form-visible":"model.fullheight != 'true'"} */
+	public String getToppadding() {
+		return toppadding;
+	}
+
+	/* {"type":"string","x-source":"inject","x-form-label":"Bottom Padding","x-form-type":"materialrange","x-form-min":0,"x-form-max":120,"x-form-visible":"model.fullheight != 'true'"} */
+	public String getBottompadding() {
+		return bottompadding;
+	}
+
+
+//GEN]
+
+    //GEN[:CUSTOMGETTERS
+    //GEN]
+
+}
---
+++ core/src/main/java/com/themecleanflex/models/PagelistnestedModel.java
@@ -0,0 +1,58 @@
+package com.themeclean.models;
+
+import com.peregrine.nodetypes.models.AbstractComponent;
+import com.peregrine.nodetypes.models.IComponent;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
+import org.apache.sling.models.annotations.Model;
+
+/*
+    //GEN[:DATA
+    {
+  "definitions": {
+    "Pagelistnested": {
+      "type": "object",
+      "x-type": "component",
+      "properties": {}
+    }
+  },
+  "name": "Pagelistnested",
+  "componentPath": "themecleanflex/components/pagelistnested",
+  "package": "com.themecleanflex.models",
+  "modelName": "Pagelistnested",
+  "classNameParent": "AbstractComponent"
+}
+//GEN]
+*/
+
+//GEN[:DEF
+@Model(
+        adaptables = Resource.class,
+        resourceType = "themecleanflex/components/pagelistnested",
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL,
+        adapters = IComponent.class
+)
+@Exporter(
+        name = "jackson",
+        extensions = "json"
+)
+
+//GEN]
+public class PagelistnestedModel extends AbstractComponent {
+
+    public PagelistnestedModel(Resource r) { super(r); }
+
+    //GEN[:INJECT
+    
+//GEN]
+
+    //GEN[:GETTERS
+    
+//GEN]
+
+    //GEN[:CUSTOMGETTERS
+    
+    //GEN]
+
+}
