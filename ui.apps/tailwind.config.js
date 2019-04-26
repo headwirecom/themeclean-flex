7a280fba137cf8e1a142a9fa3af93bbe2c4d1fb1
djp918 <djp@headwire.com>
2019-04-26 13:46:21 -0700

    add aspect ratio utils and dont' purge css for development

--- ui.apps/tailwind.config.js
+++ ui.apps/tailwind.config.js
@@ -958,6 +958,14 @@ module.exports = {
       // center: true,
       // padding: '1rem',
     }),
+    require('tailwindcss-aspect-ratio')({
+      ratios: {
+        'square': [1, 1],
+        '16/9': [16, 9],
+        '4/3': [4, 3],
+        '21/9': [21, 9],
+      }
+    }),
   ],
 
 
