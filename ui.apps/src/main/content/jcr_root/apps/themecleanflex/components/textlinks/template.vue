27ed6e6c1a68e333c33902fc294e5a6b29d4f2ae
djp918 <djp@headwire.com>
2019-04-29 08:52:29 -0700

    updates for textlinks

--- ui.apps/src/main/content/jcr_root/apps/themecleanflex/components/textlinks/template.vue
+++ ui.apps/src/main/content/jcr_root/apps/themecleanflex/components/textlinks/template.vue
@@ -1,7 +1,7 @@
 <template>
-  <ul class="navbar-nav nav-dropdown text-right">
-    <li class="nav-item" v-for="(item,i) in model.links" :key="i">
-      <a class="nav-link" v-bind:href="$helper.pathToUrl(item.link)" v-html="item.text"
+  <ul class="list-reset flex-col md:flex-row">
+    <li v-for="(item,i) in model.links" :key="i">
+      <a class="no-underline" v-bind:href="$helper.pathToUrl(item.link)" v-html="item.text"
       v-bind:class="{'text-dark' : model.colorscheme === 'light','text-light' : model.colorscheme === 'dark'}"></a>
     </li>
   </ul>
