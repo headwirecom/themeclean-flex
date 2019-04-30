76b6e3e71939bac1459b3103f87dd1555e513c36
djp918 <djp@headwire.com>
2019-04-30 12:47:56 -0700

    remove aritcle components

--- fragments/articlemediafull/htmltovue.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    convert: function($, f) {
-    	f.wrap($, 'themecleanflex-components-block')
-        f.bindAttribute($.parent(),'model','model')
-        f.replace( $.find('img'), '<themecleanflex-components-media :model="model"></themecleanflex-components-media>')
-        f.bindAttribute($.find('div').eq(1), 'style', "{flexBasis:`${model.mediawidth}%`}")
-        f.addIf($.find('.perIsEditAndEmpty').first(), 'isEditAndEmpty')
-    }
-}
--- fragments/articletwocolumn/htmltovue.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    convert: function($, f) {
-        f.wrap($, 'themecleanflex-components-block')
-        f.bindAttribute($.parent(),'model','model')
-        f.mapRichField( $.find('div').eq(0), 'model.textleft')
-        f.mapRichField( $.find('div').eq(1), 'model.textright')
-        f.addIf($.find('.perIsEditAndEmpty').first(), 'isEditAndEmpty')
-    }
-}
