ad85e9fa25ac936cab4066659a035200908e3db7
DJ Pelland <djpelland@gmail.com>
2019-05-05 21:07:00 -0700

    add transition for header menu

--- fragments/header/htmltovue.js
+++ fragments/header/htmltovue.js
@@ -3,8 +3,7 @@ module.exports = {
         //f.bindPath($)
     	f.wrap($, 'themecleanflex-components-block')
         f.bindAttribute($.parent(),'model','model')
-        f.addIf($.find('h1').first(), 'editAndEmpty')
-        f.addIf($.find('span.navbar-logo a').first(), 'model.logo')
+        f.addIf($.find('a').first(), 'model.logo')
         let navclasses = "{" +
             "'navbar-expand-lg': model.collapsed === 'false'," +
             "'navbar-light': model.colorscheme === 'light'," +
@@ -17,7 +16,15 @@ module.exports = {
     	f.bindAttribute($.find('img').first(), 'alt', 'model.logoalttext')
         f.addStyle($.find('img').first(), 'height', 'parseInt(model.logosize)', 'px')
 
-    	f.replace( $.find('nav>div div:nth-child(1)'), '<themecleanflex-components-textlinks v-bind:model="model"></themecleanflex-components-textlinks>')
-    	f.replace( $.find('nav>div div:nth-child(2)'), '<themecleanflex-components-menubuttons v-bind:model="model"></themecleanflex-components-menubuttons>')
+
+    	f.replace( $.find('nav>div>div div:nth-child(1)'), '<themecleanflex-components-textlinks v-bind:model="model"></themecleanflex-components-textlinks>')
+        f.replace( $.find('nav>div>div div:nth-child(2)'), '<themecleanflex-components-menubuttons v-bind:model="model"></themecleanflex-components-menubuttons>')
+
+        f.bindEvent( $.find('nav>div>div:nth-child(3)'), 'click', 'toggleMenu')
+
+        f.addStyle( $.find('nav>div:nth-child(2)'), 'height', "menuActive ? menuHeight + 'px' : '0px'")
+
+    	f.replace( $.find('nav>div:nth-child(2) div>div:nth-child(1)'), '<themecleanflex-components-textlinks v-bind:model="model"></themecleanflex-components-textlinks>')
+        f.replace( $.find('nav>div:nth-child(2) div>div:nth-child(2)'), '<themecleanflex-components-menubuttons v-bind:model="model"></themecleanflex-components-menubuttons>')
     }
 }
--- ui.apps/src/main/content/jcr_root/apps/themecleanflex/components/header/template.vue
+++ ui.apps/src/main/content/jcr_root/apps/themecleanflex/components/header/template.vue
@@ -1,16 +1,27 @@
 <template>
   <themecleanflex-components-block v-bind:model="model">
-    <nav class="flex w-full justify-between" v-bind:class="{'navbar-expand-lg': model.collapsed === 'false','navbar-light': model.colorscheme === 'light','navbar-dark': model.colorscheme === 'dark'}">
-      <!-- Logo -->
-      <a v-bind:href="$helper.pathToUrl(model.logourl)">
-        <img v-bind:src="$helper.pathToUrl(model.logo)" v-bind:alt="model.logoalttext"
-        v-bind:style="`height:${parseInt(model.logosize)}px;`">
-      </a>
-      <!-- TODO Mobile Hamburger <!-- Collapsible Menu -->
-      <div class="flex justify-end">
-        <themecleanflex-components-textlinks v-bind:model="model"></themecleanflex-components-textlinks>
-        <themecleanflex-components-menubuttons
-        v-bind:model="model"></themecleanflex-components-menubuttons>
+    <nav class="w-full" v-bind:class="{'navbar-expand-lg': model.collapsed === 'false','navbar-light': model.colorscheme === 'light','navbar-dark': model.colorscheme === 'dark'}">
+      <div class="flex w-full justify-between items-center">
+        <!-- Logo -->
+        <a v-if="model.logo" v-bind:href="$helper.pathToUrl(model.logourl)">
+          <img v-bind:src="$helper.pathToUrl(model.logo)" v-bind:alt="model.logoalttext"
+          v-bind:style="`height:${parseInt(model.logosize)}px;`">
+        </a>
+        <!-- Collapsible Menu -->
+        <div class="hidden md:flex justify-end">
+          <themecleanflex-components-textlinks v-bind:model="model"></themecleanflex-components-textlinks>
+          <themecleanflex-components-menubuttons
+          v-bind:model="model"></themecleanflex-components-menubuttons>
+        </div>
+        <div class="btn btn-lg btn-white cursor-pointer block md:hidden transform-rotate-90"
+        v-on:click="toggleMenu">|||</div>
+      </div>
+      <div class="flex flex-col w-full md:hidden transition-height" v-bind:style="`height:${menuActive ? menuHeight + 'px' : '0px'};`">
+        <div ref="autoHeight" class="p-4">
+          <themecleanflex-components-textlinks v-bind:model="model"></themecleanflex-components-textlinks>
+          <themecleanflex-components-menubuttons
+          v-bind:model="model"></themecleanflex-components-menubuttons>
+        </div>
       </div>
     </nav>
   </themecleanflex-components-block>
@@ -19,11 +30,25 @@
 <script>
     export default {
         props: ['model'],
+        data: function() {
+          return {
+            menuActive: false,
+            menuHeight: 0
+          }
+        },
+        mounted: function() {
+          this.menuHeight = this.$refs.autoHeight.clientHeight;
+        },
         computed: {
         	isEditAndEmpty() {
                 if(!$peregrineApp.isAuthorMode()) return false
                 return this.$helper.areAllEmpty(this.model.logo, this.model.links,  this.model.buttons)
             }
+        },
+        methods: {
+          toggleMenu: function(){
+            this.menuActive = !this.menuActive
+          }
         }
     }
 </script>
