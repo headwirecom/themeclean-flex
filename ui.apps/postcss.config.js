0bff9f3cb56631f248d1167d02d8e7cf11075289
djp918 <djp@headwire.com>
2019-05-06 08:49:57 -0700

    add dialogs to tailwind extraction

--- ui.apps/postcss.config.js
+++ ui.apps/postcss.config.js
@@ -1,9 +1,26 @@
 const purgecss = require('@fullhuman/postcss-purgecss')
 const tailwind = require('tailwindcss');
 
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+  }
+}
+
 module.exports = {
   plugins: [
     tailwind('./tailwind.config.js'),
-    purgecss({ content: ['./src/main/content/jcr_root/**/*.vue']})
+    new purgecss({
+      content: ['./src/main/content/jcr_root/**/*.vue', './src/main/content/jcr_root/**/dialog.json'],
+      extractors: [
+        {
+          extractor: TailwindExtractor,
+
+          // Specify the file extensions to include when scanning for
+          // class names.
+          extensions: ["html", "js", "vue", "json"]
+        }
+      ]
+    })
   ]
 }
