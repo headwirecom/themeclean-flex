3b791e337d4220092e85d33b24377a8ba8c9a6ed
djp918 <djp@headwire.com>
2019-05-07 14:50:57 -0700

    improve textlinks styles

--- ui.apps/src/main/content/jcr_root/apps/themecleanflex/components/textlinks/template.vue
+++ ui.apps/src/main/content/jcr_root/apps/themecleanflex/components/textlinks/template.vue
@@ -1,7 +1,7 @@
 <template>
   <ul class="list-reset flex flex-col items-center md:flex-row">
-    <li v-for="(item,i) in model.links" :key="i">
-      <a class="no-underline p-4 hover:underline" v-bind:href="$helper.pathToUrl(item.link)"
+    <li class="m-3" v-for="(item,i) in model.links" :key="i">
+      <a class="no-underline p-3 hover:underline" v-bind:href="$helper.pathToUrl(item.link)"
       v-html="item.text" v-bind:class="{'text-dark' : model.colorscheme === 'light','text-light' : model.colorscheme === 'dark','font-bold' : $data.path === $helper.pathToUrl(item.link)}"></a>
     </li>
   </ul>
