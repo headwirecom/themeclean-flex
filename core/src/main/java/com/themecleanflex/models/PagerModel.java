cc3f97144d4bfff4987b4fe11e66327eca4e2d5e
djp918 <djp@headwire.com>
2019-05-16 11:09:10 -0700

    fix pager model

--- core/src/main/java/com/themecleanflex/models/PagerModel.java
+++ core/src/main/java/com/themecleanflex/models/PagerModel.java
@@ -1,17 +1,24 @@
 package com.themecleanflex.models;
 
+import com.peregrine.adaption.PerPage;
 import com.peregrine.nodetypes.models.AbstractComponent;
 import com.peregrine.nodetypes.models.IComponent;
 import com.peregrine.nodetypes.models.Container;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.List;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+
 
 /*
     //GEN[:DATA
@@ -227,8 +234,12 @@ import javax.inject.Named;
         extensions = "json"
 )
 
+
+
 //GEN]
 public class PagerModel extends AbstractComponent {
+
+  private static final Logger LOG = LoggerFactory.getLogger(PagerModel.class);
 
     public PagerModel(Resource r) { super(r); }
 
@@ -404,5 +415,47 @@ public class PagerModel extends AbstractComponent {
 
     //GEN[:CUSTOMGETTERS
     //GEN]
+    public String getPrevious() {
+      Resource res = getCurrentPage(getRootResource());
+      LOG.debug("resource: {}",res);
+      if(res == null) res = getCurrentPage(getResource());
+      PerPage page = res.adaptTo(PerPage.class);
+      if(page == null) return "not adaptable";
+      PerPage prev = page.getPrevious();
+      return prev != null ? prev.getPath(): "unknown";
+    }
+
+    public String getNext() {
+      Resource res = getCurrentPage(getRootResource());
+      if(res == null) res = getCurrentPage(getResource());
+      PerPage page = res.adaptTo(PerPage.class);
+      if(page == null) return "not adaptable";
+      PerPage next = page.getNext();
+      return next != null ? next.getPath(): "unknown";
+    }
+    
+    private Resource getCurrentPage(Resource resource) {
+      if(resource == null) { return null; }
+      String resourceType = null;
+      try{
+        
+        ValueMap props = resource.adaptTo(ValueMap.class);
+        resourceType = props.get("jcr:primaryType", "type not found");
+        LOG.debug("resource type is: " + resourceType + "  path is:" + resource.getPath());
+        // we only care about per:page node
+        if("per:Page".equals(resourceType)) {
+          LOG.debug("returned resource type is: " + resourceType + "  path is:" + resource.getPath());
+          return resource;
+        }
+        else {
+          if(resource.getParent() != null) {
+            return getCurrentPage(resource.getParent());
+          }
+        }
+      } catch(Exception e){
+          LOG.error("Exception: " + e);
+      }
+      return null;
+    }
 
 }
