8bf76a4309e1a388e6cf1cf4275efa5aeb03c62a
djp918 <djp@headwire.com>
2019-05-17 11:27:20 -0700

    fix image display

--- ui.apps/src/main/content/jcr_root/apps/themecleanflex/components/carousel/template.vue
+++ ui.apps/src/main/content/jcr_root/apps/themecleanflex/components/carousel/template.vue
@@ -6,7 +6,7 @@
     v-bind:loop="model.wrap === 'true'" v-bind:autoplayTimeout="model.interval * 1000"
     v-bind:perPage="1">
       <slide v-for="(item,i) in model.slides" :key="i">
-        <div class="relative flex flex-col" v-bind:style="`height:${model.carouselheight}vh;`">
+        <div class="relative" v-bind:style="`height:${model.carouselheight}vh;`">
           <img class="flex-grow object-cover object-center" v-if="item.imagepath"
           v-bind:src="item.imagepath" v-bind:alt="item.alt">
           <figcaption class="absolute bottom-0 p-4 text-white text-xl w-full" v-if="item.text"
