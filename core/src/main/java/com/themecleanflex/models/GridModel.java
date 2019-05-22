977098ab18a260b07dea9a59faaf66652d1cd5fb
djp918 <djp@headwire.com>
2019-05-22 11:12:10 -0700

    initial grid

---
+++ core/src/main/java/com/themecleanflex/models/GridModel.java
@@ -0,0 +1,62 @@
+package com.themecleanflex.models;
+
+import com.peregrine.nodetypes.models.AbstractComponent;
+import com.peregrine.nodetypes.models.IComponent;
+import com.peregrine.nodetypes.models.Container;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
+import org.apache.sling.models.annotations.Model;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/*
+    //GEN[:DATA
+    {
+  "definitions": {
+    "Grid": {
+      "type": "object",
+      "x-type": "container"
+    }
+  },
+  "name": "Grid",
+  "componentPath": "themecleanflex/components/grid",
+  "package": "com.themecleanflex.models",
+  "modelName": "Grid",
+  "classNameParent": "AbstractComponent"
+}
+//GEN]
+*/
+
+//GEN[:DEF
+@Model(
+        adaptables = Resource.class,
+        resourceType = "themecleanflex/components/grid",
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL,
+        adapters = IComponent.class
+)
+@Exporter(
+        name = "jackson",
+        extensions = "json"
+)
+
+//GEN]
+public class GridModel extends AbstractComponent {
+
+    public GridModel(Resource r) { super(r); }
+
+    //GEN[:INJECT
+    
+//GEN]
+
+    //GEN[:GETTERS
+    
+//GEN]
+
+    //GEN[:CUSTOMGETTERS
+    //GEN]
+
+}
