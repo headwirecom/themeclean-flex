a843d0f6b546f3470df17383ada90e58377b4c84
djp918 <djp@headwire.com>
2019-06-28 07:49:54 -0700

    fix container template

--- fragments/containerblock/template.vue
+++ fragments/containerblock/template.vue
@@ -1,4 +1,14 @@
-<template></template>
+<template>
+  <themecleanflex-components-block v-bind:model="model">
+    <div v-bind:data-per-path="model.path">
+      <pagerender-vue-components-placeholder v-bind:model="{ path: model.path, component: model.component, location: 'before' }"></pagerender-vue-components-placeholder>
+      <template v-for="child in model.children">
+        <component v-bind:is="child.component" v-bind:model="child"></component>
+      </template>
+      <pagerender-vue-components-placeholder v-bind:model="{ path: model.path, component: model.component, location: 'after' }"></pagerender-vue-components-placeholder>
+    </div>
+  </themecleanflex-components-block>
+</template>
 
 <script>
     export default {
