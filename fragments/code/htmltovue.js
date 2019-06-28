c5011b556b7f25179c64f097824e41e3cd1078d7
Ruben Reusser <rr@headwire.com>
2019-06-28 14:03:15 -0700

    first draft of code component

---
+++ fragments/code/htmltovue.js
@@ -0,0 +1,12 @@
+module.exports = {
+    convert: function($, f) {
+        // make the element a block
+        f.wrap($, 'themecleanflex-components-block')
+        f.bindAttribute($.parent(),'model','model')
+
+        // inject the code
+        f.bindAttribute($.find('code').first(), "class", "model.language")
+        f.mapField($.find('code').first(), "model.text")
+
+    }
+}
---
+++ fragments/code/template.vue
@@ -0,0 +1,12 @@
+<template>
+  <themecleanflex-components-block v-bind:model="model">
+    <pre><code v-bind:class="model.language">{{model.text}}</code></pre>
+  </themecleanflex-components-block>
+</template>
+
+<script>
+    export default {
+        props: ['model']
+    }
+</script>
+
