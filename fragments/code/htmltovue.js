860ced26936a31ed3b6da5a31f528fe3aabc6a8c
Ruben Reusser <rr@headwire.com>
2019-07-01 09:29:11 -0700

    fix for theme of prism

--- fragments/code/htmltovue.js
+++ fragments/code/htmltovue.js
@@ -1,11 +1,14 @@
 module.exports = {
     convert: function($, f) {
+        f.bindAttribute($, "class", "'language-'+model.language+ ((model.classes) ? ' '+model.classes : '')")
+
         // make the element a block
         f.wrap($, 'themecleanflex-components-block')
         f.bindAttribute($.parent(),'model','model')
 
+
         // inject the code
-        f.bindAttribute($.find('code').first(), "class", "'language-'+model.language+ ((model.classes) ? ' '+model.classes : '')")
+        f.bindAttribute($.find('code').first(), "class", "'language-'+model.language")
         f.mapRichField($.find('code').first(), "code")
 
     }
