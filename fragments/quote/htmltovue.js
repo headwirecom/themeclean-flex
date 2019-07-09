b2e36f5de50d217399a5ac491e2bc15eb36e881b
djp918 <djp@headwire.com>
2019-07-09 08:24:21 -0700

    quote fixes

--- fragments/quote/htmltovue.js
+++ fragments/quote/htmltovue.js
@@ -5,15 +5,13 @@ module.exports = {
         f.mapRichField( $.find('div'), 'model.text')
 
         const quoteClasses = `{
-            'p-4 border-gray-800 border-l-8': model.blockquote === "true",
-            'p-4 border-gray-800 border-t-2 border-b-4': model.blockquote === "false",
-            'p-4 border-gray-800 border-l-8': model.blockquote === "true" && model.colorscheme === "light",
-            'p-4 border-gray-800 border-t-2 border-b-4': model.blockquote === "false" && model.colorscheme === "light",
-            'p-4 border-gray-200 border-l-8': model.blockquote === "true" && model.colorscheme === "dark",
-            'p-4 border-gray-200 border-t-2 border-b-4': model.blockquote === "false" && model.colorscheme === "dark",
+            'border-l-8': model.blockquote === "true",
+            'border-t-2 border-b-4': model.blockquote === "false",
+            'border-gray-800': model.colorscheme === "light",
+            'border-gray-200': model.colorscheme === "dark"
         }`
 
-        f.bindAttribute( $.find('div'), 'class', quoteClasses)
+        f.bindAttribute( $.find('div'), 'class', quoteClasses, false)
 
 		$.parent().append('<div v-if="isEditAndEmpty">no content defined for component</div>')
     }
--- fragments/quote/template.vue
+++ fragments/quote/template.vue
@@ -1,13 +1,11 @@
 <template>
   <themecleanflex-components-block v-bind:model="model">
     <div class="w-full flex justify-center">
-      <div v-html="model.text" v-bind:class="{
-            'p-4 border-gray-800 border-l-8': model.blockquote === &quot;true&quot;,
-            'p-4 border-gray-800 border-t-2 border-b-4': model.blockquote === &quot;false&quot;,
-            'p-4 border-gray-800 border-l-8': model.blockquote === &quot;true&quot; &amp;&amp; model.colorscheme === &quot;light&quot;,
-            'p-4 border-gray-800 border-t-2 border-b-4': model.blockquote === &quot;false&quot; &amp;&amp; model.colorscheme === &quot;light&quot;,
-            'p-4 border-gray-200 border-l-8': model.blockquote === &quot;true&quot; &amp;&amp; model.colorscheme === &quot;dark&quot;,
-            'p-4 border-gray-200 border-t-2 border-b-4': model.blockquote === &quot;false&quot; &amp;&amp; model.colorscheme === &quot;dark&quot;,
+      <div class="p-6 border-gray-800" v-html="model.text" v-bind:class="{
+            'border-l-8': model.blockquote === &quot;true&quot;,
+            'border-t-2 border-b-4': model.blockquote === &quot;false&quot;,
+            'border-gray-800': model.colorscheme === &quot;light&quot;,
+            'border-gray-200': model.colorscheme === &quot;dark&quot;
         }"></div>
     </div>
     <div v-if="isEditAndEmpty">no content defined for component</div>
