64b73eedb39bc23cbd61be2bc082aa7bbcfdc023
djp918 <djp@headwire.com>
2019-07-29 11:10:35 -0700

    bring in containerblock component

---
+++ fragments/containerblock/htmltovue.js
@@ -0,0 +1,10 @@
+module.exports = {
+    convert: function($, f) {
+        f.wrap($, 'themecleanflex-components-block')
+        f.bindPath($)
+        f.addChildren($)
+        f.addPlaceholders($)
+        f.bindAttribute($.parent(),'model','model')
+        f.bindAttribute($,'class','model.alignitems', false)
+    }
+}
---
+++ fragments/containerblock/template.vue
@@ -0,0 +1,18 @@
+<template>
+  <themecleanflex-components-block v-bind:model="model">
+    <div class="flex flex-wrap w-full" v-bind:data-per-path="model.path" v-bind:class="model.alignitems">
+      <pagerender-vue-components-placeholder v-bind:model="{ path: model.path, component: model.component, location: 'before' }"></pagerender-vue-components-placeholder>
+      <template v-for="child in model.children">
+        <component v-bind:is="child.component" v-bind:model="child"></component>
+      </template>
+      <pagerender-vue-components-placeholder v-bind:model="{ path: model.path, component: model.component, location: 'after' }"></pagerender-vue-components-placeholder>
+    </div>
+  </themecleanflex-components-block>
+</template>
+
+<script>
+    export default {
+        props: ['model']
+    }
+</script>
+
