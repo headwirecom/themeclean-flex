9d5119b9826313d2cde3781c4b21ce140e801f19
djp918 <djp@headwire.com>
2019-07-29 11:18:35 -0700

    add tailwind widths to whitelist

--- ui.apps/postcss.config.js
+++ ui.apps/postcss.config.js
@@ -12,7 +12,7 @@ module.exports = {
     tailwind('./tailwind.config.js'),
     new purgecss({
       content: ['./src/main/content/jcr_root/**/*.vue', './src/main/content/jcr_root/**/dialog.json', './src/main/content/jcr_root/**/*html'],
-      whitelist: ['body'],
+      whitelist: ['body', 'w-auto', 'w-full', "w-1/2", "w-1/3", "w-2/3", "w-1/4", "w-2/4", "w-3/4", "w-1/5", "w-2/5", "w-3/5", "w-4/5", "w-1/6", "w-2/6", "w-3/6", "w-4/6", "w-5/6", "w-1/12", "w-2/12", "w-3/12", "w-4/12", "w-5/12", "w-6/12", "w-7/12", "w-8/12", "w-9/12", "w-10/12", "w-11/12"],
       extractors: [
         {
           extractor: TailwindExtractor,
@@ -25,3 +25,4 @@ module.exports = {
     })
   ]
 }
+
