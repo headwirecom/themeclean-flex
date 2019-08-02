3a3f64a2519cf54b908636bc1243d48dcfc81898
djp918 <djp@headwire.com>
2019-08-02 10:20:38 -0700

    fix dropdown hover function

--- fragments/navigation/htmltovue.js
+++ fragments/navigation/htmltovue.js
@@ -5,17 +5,20 @@ module.exports = {
 
         let rootItem = $.find('div').eq(0);
         f.addFor( rootItem, 'model.childrenPages', 'child' );
+        f.bindEvent(rootItem, 'mouseover', 'showDropDown(i)');
+        f.bindEvent(rootItem, 'mouseout', 'hideDropDown(i)');
 
         let rootLink = $.find('a');
         f.mapField(rootLink, 'child.title');
         f.bindAttribute(rootLink, 'href', 'child.path');
         
         let nested = $.find('ul');
-        f.addFor( nested, 'child.childrenPages', 'subchild' );
         f.addIf(nested, 'child.hasChildren && child.childrenPages && child.childrenPages.length > 0')
+        f.bindAttribute( nested, 'class', "active[i] ? 'visible':'invisible'", false)
 
         let nestedLink = $.find('a').eq(1);
-        f.mapField(nestedLink, 'subchild.title');
+        f.addFor( nestedLink, 'child.childrenPages', 'subchild' );
+        f.mapField( nestedLink, 'subchild.title' );
         f.bindAttribute(nestedLink, 'href', 'subchild.path');
 
 		$.append('<div v-if="isEditAndEmpty">no content defined for component</div>')
--- ui.apps/src/main/content/jcr_root/apps/themecleanflex/components/navigation/template.vue
+++ ui.apps/src/main/content/jcr_root/apps/themecleanflex/components/navigation/template.vue
@@ -1,12 +1,15 @@
 <template>
   <themecleanflex-components-block v-bind:model="model">
     <nav class="flex">
-      <div class="flex flex-col" v-for="(child,i) in model.childrenPages" :key="i">
+      <div class="flex flex-col" v-for="(child,i) in model.childrenPages" :key="i"
+      v-on:mouseover="showDropDown(i)" v-on:mouseout="hideDropDown(i)">
         <a v-bind:href="child.path">{{child.title}}</a>
-        <ul class="flex flex-col" v-for="(subchild,i) in child.childrenPages"
-        :key="i" v-if="child.hasChildren &amp;&amp; child.childrenPages &amp;&amp; child.childrenPages.length &gt; 0">
-          <a v-bind:href="subchild.path">{{subchild.title}}</a>
+        <ul class="flex flex-col" v-if="child.hasChildren &amp;&amp; child.childrenPages &amp;&amp; child.childrenPages.length &gt; 0"
+        v-bind:class="active[i] ? 'visible':'invisible'">
+          <a v-bind:href="subchild.path" v-for="(subchild,i) in child.childrenPages"
+          :key="i">{{subchild.title}}</a>
         </ul>
+        <div></div>
       </div>
       <div v-if="isEditAndEmpty">no content defined for component</div>
     </nav>
@@ -15,7 +18,22 @@
 
 <script>
     export default {
-        props: ['model']
+        props: ['model'],
+        data: function() {
+          const numElements = this.model.childrenPages ? this.model.childrenPages.length : 0;
+          return {
+            active: new Array(numElements).fill(false),
+          }
+        },
+        methods: {
+          showDropDown: function(i){
+            Vue.set(this.active, i, true)
+          },
+          hideDropDown: function(i){
+            Vue.set(this.active, i, false)
+          }
+        }
+
     }
 </script>
 
