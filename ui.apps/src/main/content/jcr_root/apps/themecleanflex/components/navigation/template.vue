f5667f93c34da4f9c86b84068ad385091173b46d
djp918 <djp@headwire.com>
2019-08-05 08:02:52 -0700

    add beforeSave method to delete childrenPages

--- ui.apps/src/main/content/jcr_root/apps/themecleanflex/components/navigation/template.vue
+++ ui.apps/src/main/content/jcr_root/apps/themecleanflex/components/navigation/template.vue
@@ -18,7 +18,13 @@
 
 <script>
     export default {
-        props: ['model']
+        props: ['model'],
+        methods: {
+            beforeSave(data) {
+                delete data.childrenPages
+                return data
+            }
+        }
     }
 </script>
 
