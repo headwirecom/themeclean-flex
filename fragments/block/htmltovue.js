f066d899655d947d48876fd08577dca1fe960a93
Ruben Reusser <rr@headwire.com>
2019-08-05 08:27:59 -0700

    added component class names to all components

--- fragments/block/htmltovue.js
+++ fragments/block/htmltovue.js
@@ -1,6 +1,6 @@
 module.exports = {
     convert: function($, f) {
-        f.bindAttribute( $, 'class', '[classes, colors]', false)
+        f.bindAttribute( $, 'class', '[classes, colors, model.component]', false)
         f.bindAttribute( $, 'style', '[styles, sticky]', false)
         f.bindAttribute( $, 'data-per-path', 'model.path')
         f.bindAttribute( $, 'is', 'model.htmlelement || "div"')
