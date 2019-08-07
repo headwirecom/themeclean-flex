033c581635a7318a9aa0aafe5889756c73ff9553
djp918 <djp@headwire.com>
2019-08-07 13:05:04 -0700

    remove unused anchor

--- fragments/block/template.vue
+++ fragments/block/template.vue
@@ -3,7 +3,6 @@
   v-bind:class="[classes, colors, model.component]" v-bind:style="[styles, sticky]"
   v-bind:data-per-path="model.path" v-bind:is="model.htmlelement || &quot;div&quot;"
   v-bind:id="model.anchorname">
-    <a ref="anchor"></a>
     <div class="embed-responsive embed-responsive-16by9 w-full h-full top-0 left-0"
     v-if="model.custombackground === 'true' &amp;&amp; model.backgroundtype == 'video' &amp;&amp; model.bgvideo"
     v-bind:style="`position:${'absolute'};pointer-events:${'none'};`">
