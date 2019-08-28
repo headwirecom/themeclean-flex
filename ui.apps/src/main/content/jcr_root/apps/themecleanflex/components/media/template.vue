d41d61241eb2a25af4bbd30b8ff6a0812bb08f07
djp918 <djp@headwire.com>
2019-08-28 09:44:34 -0700

    add media lazy load

--- ui.apps/src/main/content/jcr_root/apps/themecleanflex/components/media/template.vue
+++ ui.apps/src/main/content/jcr_root/apps/themecleanflex/components/media/template.vue
@@ -2,7 +2,7 @@
   <div class="w-full">
     <h3 v-if="noMedia" class="w-full text-center">No media content</h3>
     <div v-else-if="model.mediatype === 'video'" class="relative block w-full p-0 overflow-hidden aspect-ratio-16/9">
-      <iframe :title="model.mediatitle" class="absolute inset-0 w-full h-full" :src="$helper.pathToUrl(model.videosrc)" frameborder="0" allowfullscreen></iframe>
+      <iframe :title="model.mediatitle" class="absolute inset-0 w-full h-full" :src="videoSource" frameborder="0" allowfullscreen></iframe>
     </div>
     <v-lazy-image v-else-if="model.mediatype === 'image'" class="w-full" :src="$helper.pathToUrl(model.imagesrc)" :alt="model.mediatitle"></v-lazy-image> 
   </div>
@@ -12,6 +12,16 @@
   import VLazyImage from 'v-lazy-image';
   export default {
     props: ["model"],
+    data: function() {
+      return {
+        videoSource: null
+      }
+    },
+    mounted: function() {
+      this.$nextTick(function () {
+        this.videoSource = this.$helper.pathToUrl(this.model.videosrc)
+      })
+    },
     computed: {
       noMedia() {
         let { mediatype, videosrc, imagesrc } = this.model
