9cf624c6e96891f5e44cd2cc757534a6764ecdc7
Muzaffar Nurmukhammadov <muzaffar.nurmukhammadov@cybercon.de>
2020-03-23 17:40:54 +0100

    fix broken pagerendervue reference in vue components

--- ui.apps/src/main/content/jcr_root/apps/themecleanflex/components/containerblock/template.vue
+++ ui.apps/src/main/content/jcr_root/apps/themecleanflex/components/containerblock/template.vue
@@ -1,11 +1,11 @@
 <template>
   <themecleanflex-components-block v-bind:model="model">
     <div class="flex flex-wrap w-full" v-bind:data-per-path="model.path" v-bind:class="model.alignitems">
-      <pagerender-vue-components-placeholder v-bind:model="{ path: model.path, component: model.component, location: 'before' }"></pagerender-vue-components-placeholder>
+      <pagerendervue-components-placeholder v-bind:model="{ path: model.path, component: model.component, location: 'before' }"></pagerendervue-components-placeholder>
       <template v-for="child in model.children">
         <component v-bind:is="child.component" v-bind:model="child" v-bind:key="child.path"></component>
       </template>
-      <pagerender-vue-components-placeholder v-bind:model="{ path: model.path, component: model.component, location: 'after' }"></pagerender-vue-components-placeholder>
+      <pagerendervue-components-placeholder v-bind:model="{ path: model.path, component: model.component, location: 'after' }"></pagerendervue-components-placeholder>
     </div>
   </themecleanflex-components-block>
 </template>
