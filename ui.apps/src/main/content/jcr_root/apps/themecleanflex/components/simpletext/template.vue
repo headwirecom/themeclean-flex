31346d7d3960e1b74ba6e01bb14702bcb9341db3
Radosław Wesołowski <radoslaw.wesolowski@gmail.com>
2020-03-27 12:06:34 +0100

    Make simple texts inlined in simpletext

--- ui.apps/src/main/content/jcr_root/apps/themecleanflex/components/simpletext/template.vue
+++ ui.apps/src/main/content/jcr_root/apps/themecleanflex/components/simpletext/template.vue
@@ -2,13 +2,14 @@
   <themecleanflex-components-block v-bind:model="model">
     <div class="p-5" v-if="isEditAndEmpty">no content defined for component</div>
     <component v-bind:is="model.element"
+    v-html="model.text" data-per-inline-property="model.text" data-per-inline-is-rich="false"
     v-bind:class="{
             'text-4xl font-semibold': model.element === 'h1',
             'text-3xl font-semibold': model.element === 'h2',
             'text-2xl': model.element === 'h3',
             'text-xl font-semibold': model.element === 'h4',
             'text-lg': model.element === 'h5',
-        }" v-else>{{model.text}}</component>
+        }" v-else></component>
   </themecleanflex-components-block>
 </template>
 
