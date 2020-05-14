622828a2fb72315eea9d4c6083817fc60ef4c200
smcgrath0 <smcgrath7777@gmail.com>
2020-05-14 15:52:46 -0700

    #78 no conflict

--- ui.apps/src/main/content/jcr_root/apps/themecleanflex/components/tags/template.vue
+++ ui.apps/src/main/content/jcr_root/apps/themecleanflex/components/tags/template.vue
@@ -4,7 +4,7 @@
     <div class="w-full flex flex-wrap items-center -mx-2"
     v-else>
       <span class="font-bold ml-2 mr-4 mb-1" v-if="model.tagslabel">{{model.tagslabel}}</span>
-      <component class="text-white hover:text-white px-4 py-1 mx-2 rounded-full shadow-md no-underline mb-1"
+      <component class="text-white hover:text-white px-4 py-1 mx-2 mb-1 rounded-full shadow-md no-underline"
       v-for="(item, i) in tags" :key="i" v-bind:class="{
             'bg-blue-600': model.tagcolor === &quot;blue&quot;,
             'bg-green-600': model.tagcolor === &quot;green&quot;,
