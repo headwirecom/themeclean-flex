57801d4caee6c7eb35733c41fc945a0d5fb6cc0d
ruben Reusser <rr@headwire.com>
2020-07-14 09:02:01 -0700

    merged placeholder start/end if no children are present and hiding placeholder if it is from a model and its children are also from a model

--- ui.apps/src/main/content/jcr_root/apps/themecleanflex/components/containerblock/template.vue
+++ ui.apps/src/main/content/jcr_root/apps/themecleanflex/components/containerblock/template.vue
@@ -1,18 +1,32 @@
 <template>
   <themecleanflex-components-block v-bind:model="model">
     <div class="flex flex-wrap w-full" v-bind:data-per-path="model.path" v-bind:class="model.alignitems">
-      <pagerendervue-components-placeholder v-bind:model="{ path: model.path, component: model.component, location: 'before' }"></pagerendervue-components-placeholder>
+      <pagerendervue-components-placeholder v-if="renderPlaceholder" v-bind:model="{ path: model.path, component: model.component, location: 'before' }"></pagerendervue-components-placeholder>
+      <pagerendervue-components-placeholder v-if="renderPlaceholderEmpty" class="per-drop-target-empty" v-bind:model="{ path: model.path, component: 'container block: drop components here', location: 'into' }"></pagerendervue-components-placeholder>
       <template v-for="child in model.children">
         <component v-bind:is="child.component" v-bind:model="child" v-bind:key="child.path"></component>
       </template>
-      <pagerendervue-components-placeholder v-bind:model="{ path: model.path, component: model.component, location: 'after' }"></pagerendervue-components-placeholder>
+      <pagerendervue-components-placeholder v-if="renderPlaceholder" v-bind:model="{ path: model.path, component: model.component, location: 'after' }"></pagerendervue-components-placeholder>
     </div>
   </themecleanflex-components-block>
 </template>
 
 <script>
     export default {
-        props: ['model']
+        props: ['model'],
+        computed: {
+          renderPlaceholderEmpty() {
+              return this.model.children.length === 0
+          },
+          renderPlaceholder() {
+              if( this.model.fromTemplate 
+                  && this.model.children.length > 0
+                  && this.model.children[0].fromTemplate) {
+                  return false
+              }
+              return !this.renderPlaceholderEmpty
+          }
+      }
     }
 </script>
 
