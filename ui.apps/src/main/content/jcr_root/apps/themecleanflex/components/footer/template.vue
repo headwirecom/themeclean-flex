3a85cad01f40d118d0b2a497fefb71c68cec722d
smcgrath0 <smcgrath7777@gmail.com>
2020-08-11 12:01:36 -0700

    #145 fixed typo in footer component

--- ui.apps/src/main/content/jcr_root/apps/themecleanflex/components/footer/template.vue
+++ ui.apps/src/main/content/jcr_root/apps/themecleanflex/components/footer/template.vue
@@ -10,9 +10,9 @@
         <div class="footer-col flex-1 lg:mx-4" v-for="(item, i) in model.columns"
         :key="item.path || i">
           <h2 class="text-lg font-bold my-2" v-if="item.title !== ''"
-          v-bind:data-per-inline="`model.clolumns.${i}.title`">{{item.title}}</h2>
+          v-bind:data-per-inline="`model.columns.${i}.title`">{{item.title}}</h2>
           <div v-if="item.text !== ''" v-html="item.text"
-          v-bind:data-per-inline="`model.clolumns.${i}.text`"></div>
+          v-bind:data-per-inline="`model.columns.${i}.text`"></div>
         </div>
       </div>
       <hr class="border-b">
