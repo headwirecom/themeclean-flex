c213bb180111f037611e25d7bfa926095f947ef9
smcgrath0 <smcgrath7777@gmail.com>
2020-08-12 13:37:24 -0700

    #149 added nested dropdowns and fixed desktop nav when scrolling

--- fragments/navigation/hatch.js
+++ fragments/navigation/hatch.js
@@ -14,13 +14,27 @@ module.exports = {
         let rootItem = $.find('div').eq(0);
         f.addFor( rootItem, 'model.childrenPages', 'child' );
 
-        let rootLink = $.find('a');
-        f.mapField(rootLink, 'child.title', "model.childrenPages", "title");
-        f.bindAttribute(rootLink, 'href', "child.childrenPages.length > 0 ? false : child.path +'.html'");
-        f.bindAttribute(rootLink, 'class', "model.colorscheme === 'dark' ? 'text-gray-200 hover:bg-gray-200 hover:text-black' : 'text-black hover:bg-black hover:text-gray-200'",false);
+        let rootLink = $.find('a').eq(0);
+        f.bindAttribute( rootLink, "aria-expanded", "`active[i] ? 'true' : 'false'`")
+
+        let allLinks = $.find('a');
+        f.mapField(allLinks, 'child.title', "model.childrenPages", "title");
+        f.bindAttribute(allLinks, 'href', "child.childrenPages.length > 0 ? false : child.path +'.html'");
+        f.bindAttribute(allLinks, 'class', "model.colorscheme === 'dark' ? 'text-gray-200 hover:bg-gray-200 hover:text-black' : 'text-black hover:bg-black hover:text-gray-200'",false);
         
-        let nested = $.find('div').eq(1);
-        f.addIf(nested, 'child.hasChildren && child.childrenPages && child.childrenPages.length > 0')
+        let nested = $.find('div').eq(0);
+        f.bindAttribute( nested, 'class', `{ 'bg-secondary': active[i] }`, false);
+
+        let nestedArrow = $.find('svg').eq(0);
+        f.addIf(nestedArrow, 'child.hasChildren && child.childrenPages && child.childrenPages.length > 0')
+        f.bindEvent(nestedArrow, 'click', "toggleItem(i)")
+        f.bindAttribute(nestedArrow, "style", "`transform:${active[i] ? 'rotate(180deg)': 'rotate(0)'};`")
+
+        let nestedListContainer = $.find("div").eq(2);
+        f.bindAttribute(nestedListContainer, "style", "`height:${active[i] ? heights[i] + 'px' : '0px'};`")
+
+        let nestedList = $.find("div").eq(3);
+        f.bindAttribute(nestedList, "ref", "`tabContent${i}`")
 
         let nestedLink = $.find('a').eq(1);
         f.addFor( nestedLink, 'child.childrenPages', 'subchild' );
--- fragments/navigation/template.vue
+++ fragments/navigation/template.vue
@@ -8,12 +8,22 @@
             'justify-end': model.justifyitems === 'end'
         }" v-else>
       <div class="flex flex-col dropdown-container" v-for="(child, i) in model.childrenPages"
-      :key="child.path || i">
-        <a class="p-3 no-underline" v-bind:data-per-inline="`model.childrenPages.${i}.title`"
-        v-bind:href="child.childrenPages.length &gt; 0 ? false : child.path +'.html'"
-        v-bind:class="model.colorscheme === 'dark' ? 'text-gray-200 hover:bg-gray-200 hover:text-black' : 'text-black hover:bg-black hover:text-gray-200'">{{child.title}}</a>
-        <div class="self-stretch" v-if="child.hasChildren &amp;&amp; child.childrenPages &amp;&amp; child.childrenPages.length &gt; 0">
-          <div class="flex flex-col dropdown-list">
+      :key="child.path || i" v-bind:class="{ 'bg-secondary': active[i] }">
+        <div class="flex justify-between md:justify-start items-center md:items-start">
+          <a class="p-3 no-underline" v-bind:aria-expanded="`active[i] ? 'true' : 'false'`"
+          v-bind:data-per-inline="`model.childrenPages.${i}.title`" v-bind:href="child.childrenPages.length &gt; 0 ? false : child.path +'.html'"
+          v-bind:class="model.colorscheme === 'dark' ? 'text-gray-200 hover:bg-gray-200 hover:text-black' : 'text-black hover:bg-black hover:text-gray-200'">{{child.title}}</a>
+          <svg width="16" height="16" viewBox="0 0 16 16" focusable="false"
+          class="block md:hidden transition-transform duration-150 ease-in-out m-3"
+          v-if="child.hasChildren &amp;&amp; child.childrenPages &amp;&amp; child.childrenPages.length &gt; 0"
+          v-on:click="toggleItem(i)" v-bind:style="`transform:${active[i] ? 'rotate(180deg)': 'rotate(0)'};`">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M13.293 4.29291L14.7072 5.70712L8.00008 12.4142L1.29297 5.70712L2.70718 4.29291L8.00008 9.5858L13.293 4.29291Z"
+            />
+          </svg>
+        </div>
+        <div class="self-stretch relative transition-height duration-200 ease-in-out overflow-hidden md:overflow-unset"
+        v-bind:style="`height:${active[i] ? heights[i] + 'px' : '0px'};`">
+          <div class="flex flex-col dropdown-list relative md:absolute" v-bind:ref="`tabContent${i}`">
             <a class="p-3 no-underline" v-bind:data-per-inline="`model.childrenPages.${i}.title`"
             v-bind:href="subchild.path + '.html'" v-bind:class="model.colorscheme === 'dark' ? 'text-gray-200 hover:bg-gray-200 hover:text-black' : 'text-black hover:bg-black hover:text-gray-200'"
             v-for="(subchild, i) in child.childrenPages" :key="subchild.path || i"
@@ -28,12 +38,43 @@
 <script>
     export default {
         props: ['model'],
+        data: function() {
+          const numElements = this.model.childrenPages ? this.model.childrenPages.length : 0;
+          return {
+            active: new Array(numElements).fill(false),
+            heights: new Array(numElements).fill(0),
+          }
+        },
+        created() {
+          addEventListener("resize", this.setHeights);
+        },
+        destroyed() {
+          removeEventListener("resize", this.setHeights);
+        },
+        mounted: function() {
+          this.setHeights();
+        },
         methods: {
             beforeSave(data) {
                 delete data.childrenPages
                 return data
+            },
+            setHeights: function() {
+              this.heights.forEach( (item,i) => {
+                Vue.set(this.heights, i, this.$refs[`tabContent${i}`][0].clientHeight )}
+              );
+            },
+            toggleItem(i) {
+              // if (this.model.toggletype === 'accordion') {
+              //   this.active.forEach( (active,j) => {
+              //     Vue.set(this.active, j, j === i ? !this.active[j] : false);
+              //   })
+              // } else {
+                Vue.set(this.active, i, !this.active[i])
+              // }
             }
         },
+        
         computed: {
         	isEditAndEmpty() {
             if(!$peregrineApp.isAuthorMode()) return false;
@@ -46,13 +87,13 @@
 </script>
 
 <style>
+@media (min-width: 768px) {
   .flex.dropdown-list {
     display: none;
-    position: fixed;
   }
   .flex.dropdown-container:hover .flex.dropdown-list, .flex.dropdown-container:focus-within .flex.dropdown-list {
     display: flex;
   }
-  
+}
 </style>
 
