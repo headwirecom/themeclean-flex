013c5bc8fa52d9f70f52724cec5dedabdede0fa8
Felix Pütz <50131232+Felix-Puetz@users.noreply.github.com>
2021-06-05 18:35:33 +0200

    feature/peregrine-icon: (#198)
    - add peregrine-icon as custom web-component
    - add customElements.js as siteJS

---
+++ ui.apps/src/main/content/jcr_root/etc/felibs/themecleanflex/dependencies/customElements.js
@@ -0,0 +1,44 @@
+class PeregrineIcon extends HTMLElement {
+  constructor() {
+    super();
+
+    const shadowDom = this.attachShadow({ mode: 'open' });
+
+    const wrapperElem = document.createElement('span');
+    wrapperElem.setAttribute('class', 'wrapper');
+
+    const icon = document.createElement('span');
+    icon.setAttribute('class', 'icon');
+    icon.setAttribute('tabindex', 0);
+
+    let imgUrl;
+    if (this.hasAttribute('img')) {
+      imgUrl = this.getAttribute('img');
+    } else {
+      imgUrl = 'img/default.png';
+    }
+
+    const imgElem = document.createElement('img');
+    imgElem.src = imgUrl;
+    icon.appendChild(imgElem);
+
+    const styleElem = document.createElement('style');
+    styleElem.textContent = `
+      .wrapper {
+        position: relative;
+      }
+
+      img {
+        width: 1.2rem;
+      }
+    `;
+
+    shadowDom.appendChild(styleElem);
+    shadowDom.appendChild(wrapperElem);
+    wrapperElem.appendChild(icon);
+  }
+}
+
+if (!customElements.get('peregrine-icon')) {
+  customElements.define('peregrine-icon', PeregrineIcon);
+}
