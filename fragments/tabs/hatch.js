50c8538d30d43705a5572ea123330a25cf2ee72c
Michał Goliński <mg@headwire.com>
2021-12-21 15:42:42 +0100

    Change the field value in model while inline editing (#205)

--- fragments/tabs/hatch.js
+++ fragments/tabs/hatch.js
@@ -45,14 +45,14 @@ module.exports = {
     	f.bindAttribute( a, 'aria-controls', '`tab${_uid}${parseInt(i)+1}`')
     	f.bindAttribute( a, 'aria-selected', 'active === i')
     	f.bindEvent( a, 'click', 'toggleActive(i)')
-        f.mapField( a, "item.title")
+        f.mapField(a, "item.title", "model.tabs", "title")
 
     	let tabPanel = $.find('[role="tabpanel"]').first()
     	f.addFor( tabPanel, 'model.tabs')
     	f.bindAttribute(tabPanel, 'id', '`tab${_uid}${parseInt(i)+1}`')
         f.bindAttribute(tabPanel, 'aria-labelledby', '`tab-control-${_uid}${parseInt(i)+1}`')
-    	f.bindAttribute(tabPanel, 'class', "active === i ? 'block opacity-100' : 'hidden'", false)
-        f.mapRichField(tabPanel, "item.text")
+        f.bindAttribute(tabPanel, 'class', "active === i ? 'block opacity-100' : 'hidden'", false)
+        f.mapRichField(tabPanel, "item.text", "model.tabs", "text")
 
         f.addElse($);
         $.parent().prepend('<div class="p-5" v-if="isEditAndEmpty">no content defined for component</div>')
